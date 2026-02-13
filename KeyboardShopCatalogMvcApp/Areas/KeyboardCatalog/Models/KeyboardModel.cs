de7dc657ae224bc751fe9a4c909cda2fb272b4b5
Semen <semen.ruman@gmail.com>
2026-02-13 16:10:34 +0300

    Создал базовую структуру приложения. Сделал модель клавиатуры

---
+++ KeyboardShopCatalogMvcApp/Areas/KeyboardCatalog/Models/KeyboardModel.cs
@@ -0,0 +1,21 @@
+﻿namespace KeyboardShopCatalogMvcApp.Areas.KeyboardCatalog.Models
+{
+    public class KeyboardModel
+    {
+        public int Id { get; set; }
+
+        public string Brand { get; set; } // Бренд
+
+        public string Name {  get; set; } // Название
+
+        public double Price { get; set; } // Цена
+
+        public string KeyboardType { get; set; } // Тип клавиатуры (механическая, мембранная)
+
+        public double Width {  get; set; } // Ширина
+
+        public double Height { get; set; } // Высота
+
+        public int KeysCount {  get; set; } // Количество клавиш
+    }
+}
--- KeyboardShopCatalogMvcApp/Program.cs
+++ KeyboardShopCatalogMvcApp/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.Run();
