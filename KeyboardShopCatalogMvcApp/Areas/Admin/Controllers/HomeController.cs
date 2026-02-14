eba411a9f5ce873810dbb70eb9e3924aabdcb635
Semen <semen.ruman@gmail.com>
2026-02-14 16:58:02 +0300

    Начал создавать админ-панель. Добавил область Admin, контроллер и описал модель админа

---
+++ KeyboardShopCatalogMvcApp/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KeyboardShopCatalogMvcApp.Areas.Admin.Models;
+
+namespace KeyboardShopCatalogMvcApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("{area}/[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        AdminModel adminModel = new AdminModel();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Authorization()
+        {
+            return View(new AdminModel());
+        }
+
+        [HttpPost]
+        public IActionResult Authorization(AdminModel admin)
+        {
+            if (ModelState.IsValid)
+            {
+                adminModel = admin;
+                return RedirectToAction("SuccessAuthorization");
+            }
+            else
+            {
+                return View(admin);
+            }
+        }
+
+        public IActionResult SuccessAuthorization()
+        {
+            return View();
+        }
+    }
+}
---
+++ KeyboardShopCatalogMvcApp/Areas/Admin/Models/AdminModel.cs
@@ -0,0 +1,18 @@
+﻿namespace KeyboardShopCatalogMvcApp.Areas.Admin.Models
+{
+    public class AdminModel
+    {
+        private readonly string password = "adminPassword";
+        private bool isAvtorizate = false;
+        public bool IsAvtorizate { get { return isAvtorizate; } }
+
+        public void Authorization(string passwordParam)
+        {
+            if (password != passwordParam)
+            {
+                throw new Exception("Неверный пароль!");
+            }
+            isAvtorizate = true;
+        }
+    }
+}
