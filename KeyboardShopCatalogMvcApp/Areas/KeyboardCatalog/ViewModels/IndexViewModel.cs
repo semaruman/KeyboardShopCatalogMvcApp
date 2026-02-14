2583beb7100d69e21b07e5ddcada2926c0f7a6a2
Semen <semen.ruman@gmail.com>
2026-02-14 06:26:31 +0300

    Добавил модели представлений (ViewModels)

---
+++ KeyboardShopCatalogMvcApp/Areas/KeyboardCatalog/ViewModels/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace KeyboardShopCatalogMvcApp.Areas.KeyboardCatalog.ViewModels
+{
+    public class IndexViewModel
+    {
+        List<KeyboardViewModel> Keyboards { get; set; }
+        List<string> Brands { get; set; } = new List<string>() { 
+            "Все",
+            "A4Tech", "ARDOR GAMING", "Logitech", "RedSquare",
+            "Дарк Проджект", "Apple", "Defender"
+        };
+        List<string> Types { get; set; } = new List<string>()
+         {
+             "Мембранная", "Механическая", "Ножничная"
+         };
+    }
+}
---
+++ KeyboardShopCatalogMvcApp/Areas/KeyboardCatalog/ViewModels/KeyboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace KeyboardShopCatalogMvcApp.Areas.KeyboardCatalog.ViewModels
+{
+    public class KeyboardViewModel
+    {
+        public string Brand { get; set; } // Бренд
+
+        public string Name { get; set; } // Название
+
+        public double Price { get; set; } // Цена
+
+        public string KeyboardType { get; set; } // Тип клавиатуры (механическая, мембранная)
+    }
+}
